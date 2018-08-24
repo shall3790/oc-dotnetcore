701f21f7c0886974cb9eb2236a45cb126d5931e7
Stephen Hall <stephen.hall@hobbylobby.com>
2018-08-24 09:36:45 -0500

    change startup

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace oc_dotnetcore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World 2!");
             });
         }
     }
